6845e3a149c95bae5f1d43e4f914fb323847f7d3
Onella Natalie <68709851+OnellaNatalie@users.noreply.github.com>
2024-09-29 19:04:01 +0530

    Create :: UserSeeder
    Insert admin's user values

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User; 
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'), // Use bcrypt to hash the password
+            'role' => 'admin', // Set the role to admin
+            'status' => 'active', // Set the status to active
+        ]);
+    }
+}
