ad0f81be8c368dbdbd87c8f74211dbad45cb289e
Onella Natalie <68709851+OnellaNatalie@users.noreply.github.com>
2024-10-01 17:56:01 +0530

    Update auth.service.ts

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BehaviorSubject, catchError, Observable, throwError } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'http://127.0.0.1:8000/api'; // Replace with your actual API URL
-  private loggedIn = new BehaviorSubject<boolean>(false);
-  isLoggedIn$ = this.loggedIn.asObservable();
+  private auth: boolean = false;
+  private bearerToken: string = '0';
   constructor(private http: HttpClient) {}
   
 
   isAuthenticated(): boolean {
-    const user = localStorage.getItem('user');
-    return !!user; 
+    if (typeof localStorage !== 'undefined') {
+      const user = localStorage.getItem('user');
+      return !!user; 
+    }
+    return false;  
   }
 
   getRole(): string {
-    const user = JSON.parse(localStorage.getItem('user') || '{}');
-    return user.role;
+    if (typeof localStorage !== 'undefined') {
+      const user = JSON.parse(localStorage.getItem('user') || '{}');
+      return user.role;
+    }
+    return '';  
   }
 
   login(email: string, password: string): Observable<any> {
@@ -29,8 +36,26 @@ export class AuthService {
   register(data: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/register`, data);
   }
-
-  setLoggedIn(status: boolean) {
-    this.loggedIn.next(status);
+  public setUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+  
+  getUser(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/user`).pipe(
+      tap(user => {
+        console.log('Fetched user:', user);
+      })
+    );
+  }
+  public setAuth(auth: boolean){
+    this.auth  = auth;
+    localStorage.setItem('auth', ''+auth);
+  }
+  public setBearerToken(bearerToken: string){
+    this.bearerToken  = bearerToken;
+    localStorage.setItem('bearerToken', bearerToken);
+  }
+  listBooks(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/list-books`);
   }
 }
