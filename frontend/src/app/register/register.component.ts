f33543008db61f67ea520a83b3254165df7d6c6a
Onella Natalie <68709851+OnellaNatalie@users.noreply.github.com>
2024-10-01 11:38:53 +0530

    Update :: Register components

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule , HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  password_confirmation: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+
+  onSubmit(form: any) {
+    const { name, email, password, password_confirmation } = form.value;
+
+    if (password !== password_confirmation) {
+      this.errorMessage = 'Passwords do not match.';
+      return;
+    }
+
+    this.authService.register({ name, email, password, password_confirmation }).subscribe(
+      (response) => {
+        console.log('User registered successfully', response);
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.error('Registration error', error);
+        this.errorMessage = error.error?.message || 'Registration failed. Please try again.';
+      }
+    );
+  }
 }
