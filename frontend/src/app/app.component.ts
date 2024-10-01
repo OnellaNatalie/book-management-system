0914c5dfc1b70f316686f64118df0b1f5627dc6b
Onella Natalie <68709851+OnellaNatalie@users.noreply.github.com>
2024-10-01 17:53:21 +0530

    Add navbar to app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet,RouterModule} from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../app/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, HeaderComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Book Management System';
+  getUser: any;
+  destroy: any;
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  ngOnInit(): void {
+    this.getUser = this.authService.getUser();
+    console.log("user",this.getUser);
+  }
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+  logout(){
+    localStorage.clear(); // Clear local storage
+    this.authService.setAuth(false); // Update auth state
+    this.router.navigate(['/login']); 
+  }
+
 }
