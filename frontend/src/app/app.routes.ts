4348cd767a7617220bc75362fdd7c94cfe005975
Onella Natalie <68709851+OnellaNatalie@users.noreply.github.com>
2024-10-01 10:10:22 +0530

    Update ::  app.route.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AuthorDashboardComponent } from './author/author-dashboard/author-dashboard.component';
+import { adminGuard } from './guards/admin.guard';
+import { authorGuard } from './guards/author.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'login', component: LoginComponent }, 
+    { path: 'register', component: RegisterComponent }, 
+    { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [adminGuard] },
+    { path: 'author-dashboard', component: AuthorDashboardComponent,  canActivate: [authorGuard] },
+    // { path: '**', redirectTo: '' } 
+  
+];
